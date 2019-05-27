e6af5025499bfceb6b71d6fb93e5e5c8f65181f4
tamperfoxes <21273266+notwopr@users.noreply.github.com>
2019-05-26 21:46:19 -0400

    draft record new jackpots

--- testkeysearch.py
+++ testkeysearch.py
@@ -66,13 +66,34 @@ def search():
     visited.append(pagenum)
 
     # SEARCH PAGE
-        # RECORD WEBLINK
-        # STORE UPDATED VISITED LIST TO PICKLE
-        print("UPDATED VISITED LIST:\n", visited)
-        with open("visited.pkl", "wb") as targetfile:
-            pkl.dump(visited, targetfile, protocol=4)
-        # STOP
-        exit()
+    # REPORT NEW JACKPOT URL
+    jackpoturl = driver.current_url
+    print("JACKPOT! -->", jackpoturl)
+
+    #   OPEN STORED FILE OF JACKPOT PAGES
+    with open("jackpots.pkl", "rb") as targetfile:
+        jackpots_raw = pkl.load(targetfile)
+    if jackpots_raw is None:
+        # IF NONE EXISTS, CREATE OBJECT
+        jackpotlist = []
+    else:
+        jackpotlist = jackpots_raw
+        print(type(jackpotlist))
+        print("CURRENT LIST OF JACKPOT PAGES:\n {}\n\n".format(jackpotlist))
+
+    # ADD NEW JACKPOT URL TO LIST AND STORE UPDATED LIST TO PICKLE
+    jackpotlist.append(jackpoturl)
+    with open("jackpots.pkl", "wb") as targetfile:
+        pkl.dump(jackpotlist, targetfile, protocol=4)
+
+    # STORE UPDATED VISITED LIST TO PICKLE
+    print("UPDATED VISITED LIST:\n", visited)
+    with open("visited.pkl", "wb") as targetfile:
+        pkl.dump(visited, targetfile, protocol=4)
+    # STOP
+    exit()
 
 
-        # HOW TO RUN THIS IN BACKGROUND USING THE CLOUD?
+# HOW TO RUN THIS IN BACKGROUND USING THE CLOUD?
+# CAVEATS
+#   old pages may have new balances.
